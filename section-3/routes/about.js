2a4989c2987b391b0805227690adf1b7ffa0a47e
Andrew Provis <aprovis@dresources.com>
2015-05-05 15:15:33 +0100

    Worked on adding additional pages (about and contact) to learn about page routes and views (lecture 8).

--- section-3/routes/about.js
+++ section-3/routes/about.js
@@ -1,9 +1,13 @@
+/**
+ * Created by aprovis on 05/05/2015.
+ */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+    res.render('about', { title: 'About' });
 });
 
 module.exports = router;
