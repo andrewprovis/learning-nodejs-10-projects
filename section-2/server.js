c0dfea705fd946c38880ca872456704e9b44a45d
Andrew Provis <aprovis@dresources.com>
2015-05-05 12:17:40 +0100

    Completed following "Serving HTML Files" video.

--- section-2/server.js
+++ section-2/server.js
@@ -3,8 +3,53 @@
  */
 
 var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+// Array of Mime Types
+var mimeTypes = {
+    "html" : "text/html",
+    "jpeg" : "image/jpeg",
+    "jpg"  : "image/jpeg",
+    "png"  : "image/png",
+    "js"   : "text/javascript",
+    "css"  : "text/css"
+};
+
+// Create Server
+
+http.createServer(function(req, res){
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading ' + uri);
+    var stats;
+
+    try{
+        stats = fs.lstatSync(fileName);
+    } catch (e) {
+        res.writeHead(404, {'Content-type' : 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+
+    // Check if file/directory
+    if(stats.isFile()){
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+        res.writeHead(200, {'Content-type' : mimeType});
+
+        var fileStream = fs.createReadStream(fileName);
+
+        fileStream.pipe(res);
+    } else if(stats.isDirectory()){
+        res.writeHead(302, {
+            'Location' : 'index.html'
+        });
+        res.end();
+    } else {
+        res.writeHead(500, {'Content-type' : 'text/plain'});
+        res.write('500 Internal Error');
+        res.end();
+    }
+}).listen(3000);
