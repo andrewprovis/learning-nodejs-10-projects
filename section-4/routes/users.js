7268748a1f74cd807b777be972cedf6d331776c3
Andrew Provis <aprovis@dresources.com>
2015-05-06 09:31:04 +0100

    Created route to load the "Register" page.

--- section-4/routes/users.js
+++ section-4/routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register', {
+    'title': 'Register'
+  });
+});
+
 module.exports = router;
