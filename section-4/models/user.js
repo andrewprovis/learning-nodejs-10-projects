c71dbf457ecbb3ee2322d15d80bf7b726be566ad
Andrew Provis <aprovis@dresources.com>
2015-05-06 10:36:52 +0100

    Created user model file and included it with the users.js file for user registration.

---
+++ section-4/models/user.js
@@ -0,0 +1,4 @@
+/**
+ * Created by aprovis on 06/05/2015.
+ */
+
--- section-4/routes/users.js
+++ section-4/routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
