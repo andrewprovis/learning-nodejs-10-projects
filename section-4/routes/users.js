907ff6dfc3fe71a35641908c5789d581605aa632
Andrew Provis <aprovis@dresources.com>
2015-05-06 10:02:24 +0100

    Gets information passed into registration form and validates input.

--- section-4/routes/users.js
+++ section-4/routes/users.js
@@ -18,4 +18,40 @@ router.get('/login', function(req, res, next) {
   });
 });
 
+router.post('/users/register', function(req, res, next) {
+  // Get the form values
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+
+  // Check for image field
+  if(req.files.profileimage) {
+    console.log('Uploading file...');
+
+    // File info (gets the filename)
+    var profileImageOriginalName = req.files.profileimage.originalname;
+    var profileImageName = req.files.profileimage.name;
+    var profileImageMimeType = req.files.profileimage.mimetype;
+    var profileImagePath = req.files.profileimage.path;
+    var profileImageExt = req.files.profileimage.extension;
+    var profileImageSize = req.files.profileimage.size;
+  } else {
+    // Set a default image
+    var profileImageName = 'noimage.png';
+  }
+
+  // Form validation
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Not a valid email').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+});
+
+
+
 module.exports = router;
