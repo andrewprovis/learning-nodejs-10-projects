72cc534b10182ccf6c354daddaeb23e0f94248e9
Andrew Provis <aprovis@dresources.com>
2015-05-06 10:42:22 +0100

    Defined user schema

--- section-4/models/user.js
+++ section-4/models/user.js
@@ -2,3 +2,25 @@
  * Created by aprovis on 06/05/2015.
  */
 
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
+// User Schema
+var UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        index: true
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    profileimage: {
+        type: String
+    }
+});
