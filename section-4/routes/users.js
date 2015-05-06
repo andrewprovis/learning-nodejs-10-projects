da3340e26dc553f8719c0bea5ea12c27ea1f0430
Andrew Provis <aprovis@dresources.com>
2015-05-06 09:43:25 +0100

    Created route to load login page when the link is selected.

--- section-4/routes/users.js
+++ section-4/routes/users.js
@@ -12,4 +12,10 @@ router.get('/register', function(req, res, next) {
   });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+    'title': 'Login'
+  });
+});
+
 module.exports = router;
