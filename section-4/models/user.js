c89110505f806e79c4ce1c38e9c6f51d154c56ba
Andrew Provis <aprovis@dresources.com>
2015-05-06 11:32:39 +0100

    Included bcryptjs and encrypted the password before saving the new user to the database.

--- section-4/models/user.js
+++ section-4/models/user.js
@@ -3,6 +3,7 @@
  */
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
 mongoose.connect('mongodb://localhost/nodeauth');
 
@@ -15,7 +16,9 @@ var UserSchema = mongoose.Schema({
         index: true
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        bcrypt: true
     },
     email: {
         type: String
@@ -31,5 +34,14 @@ var UserSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback) {
-    newUser.save(callback);
+    bcrypt.hash(newUser.password, 10, function(err, hash) {
+        if(err) throw err;
+
+        // Set hashed password
+
+        newUser.password = hash;
+
+        // Create user
+        newUser.save(callback);
+    });
 };
