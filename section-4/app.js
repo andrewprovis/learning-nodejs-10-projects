e11f03126c788fad7a415d1cae727980be995c79
Andrew Provis <aprovis@dresources.com>
2015-05-07 12:10:46 +0100

    Handled hiding the logout button when you are not yet logged in and hides the register and login buttons when logged in.

--- section-4/app.js
+++ section-4/app.js
@@ -71,6 +71,11 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.get('*', function(req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
 
