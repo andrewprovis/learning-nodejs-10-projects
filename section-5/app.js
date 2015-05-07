5467cbed6ef48275182c9e8896e75a575122aa47
Andrew Provis <aprovis@dresources.com>
2015-05-07 14:23:47 +0100

    Setup sessions package. In real world the secret should be something a bit more secure but in practice will probably be something similar if eye witness accounts are to be believed.

--- section-5/app.js
+++ section-5/app.js
@@ -29,6 +29,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
