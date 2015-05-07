8ff051146555cac2a21acd17f22f84e6257db304
Andrew Provis <aprovis@dresources.com>
2015-05-07 11:22:50 +0100

    Handled logging in with session.

--- section-4/models/user.js
+++ section-4/models/user.js
@@ -33,12 +33,27 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+    var query = {username: username};
+    User.findOne(query, callback);
+};
+
+module.exports.getUserById = function(id, callback) {
+    User.findById(id, callback);
+};
+
 module.exports.createUser = function(newUser, callback) {
     bcrypt.hash(newUser.password, 10, function(err, hash) {
         if(err) throw err;
 
         // Set hashed password
-
         newUser.password = hash;
 
         // Create user
