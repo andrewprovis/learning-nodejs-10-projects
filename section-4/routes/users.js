8500e3daaa90b607bf6dc4af8bb2df129e8cf060
Andrew Provis <aprovis@dresources.com>
2015-05-07 12:01:38 +0100

    Fixed logout function call.

--- section-4/routes/users.js
+++ section-4/routes/users.js
@@ -132,7 +132,7 @@ router.post('/login', passport.authenticate('local', {
 });
 
 router.get('/logout', function(req, res) {
-  req.logout;
+  req.logout();
   req.flash('success', 'You have logged out');
   res.redirect('/users/login');
 });
