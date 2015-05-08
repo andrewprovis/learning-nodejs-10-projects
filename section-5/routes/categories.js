8cf00fcec9acb047353b1afc1f21011238f61b1e
Andrew Provis <aprovis@dresources.com>
2015-05-08 21:24:51 +0100

    Created post function that get category data from add categories form, validates the data and checks for errors.

--- section-5/routes/categories.js
+++ section-5/routes/categories.js
@@ -13,4 +13,22 @@ router.get('/add', function(req, res, next) {
     })
 });
 
+router.post('/add', function(req, res, next) {
+    // Get form values
+    var title       = req.body.title;
+
+    // Form validation
+    req.checkBody('title', 'Title field is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('addcategory', {
+            errors: errors,
+            "title": title
+        });
+    } else {
+    }
+});
+
 module.exports = router;
