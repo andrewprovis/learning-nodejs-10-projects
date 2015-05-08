60a262df72f30d62cb0f77dbf81cef538fd1a1dd
Andrew Provis <aprovis@dresources.com>
2015-05-08 14:20:42 +0100

    Created variables for required modules and created a function to display the add post page.

--- section-5/routes/posts.js
+++ section-5/routes/posts.js
@@ -1,3 +1,16 @@
 /**
  * Created by aprovis on 08/05/2015.
  */
+
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+router.get('/add', function(req, res, next) {
+    res.render('addpost', {
+        "title": "Add Post"
+    });
+});
+
+module.exports = router;
