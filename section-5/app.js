e06b7d9f2f51d7ccc3405e9a4496179947e42ca6
Andrew Provis <aprovis@dresources.com>
2015-05-08 09:29:55 +0100

    Removed unneeded users route and updated calles to the users route to point to the posts route instead.

--- section-5/app.js
+++ section-5/app.js
@@ -12,7 +12,7 @@ var multer = require('multer');
 var flash = require('connect-flash');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -73,7 +73,7 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
