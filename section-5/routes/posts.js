4024ec3c81ece00269b706066effa233db289868
Andrew Provis <aprovis@dresources.com>
2015-05-08 15:14:14 +0100

    Gets all submitted form values ready for validation and processing.

--- section-5/routes/posts.js
+++ section-5/routes/posts.js
@@ -13,4 +13,24 @@ router.get('/add', function(req, res, next) {
     });
 });
 
+router.post('/add', function(req, res, next) {
+    // Get form values
+    var title       = req.body.title;
+    var category    = req.body.category;
+    var body        = req.body.body;
+    var author      = req.body.author;
+    var date        = new Date();
+
+    if (req.files.mainimage) {
+        var mainImageOriginalName   = req.files.mainimage.originalname;
+        var mainImageName           = req.files.mainimage.name;
+        var mainImageMime           = req.files.mainimage.mimetype;
+        var mainImagePath           = req.files.mainimage.path;
+        var mainImageExt            = req.files.mainimage.extension;
+        var mainImageSize           = req.files.mainimage.size;
+    } else {
+        var mainImageName = 'noimage.png';
+    }
+});
+
 module.exports = router;
