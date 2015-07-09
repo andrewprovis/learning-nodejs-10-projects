6a0d35ae3974ca6a74ed12d643995070a4df85fd
Andrew Provis <aprovis@dresources.com>
2015-07-09 11:20:53 +0100

    Created add comment function to handle comments generated for blog posts.

--- section-5/routes/posts.js
+++ section-5/routes/posts.js
@@ -82,4 +82,60 @@ router.post('/add', function(req, res, next) {
     }
 });
 
+
+router.post('/addcomment', function(req, res, next) {
+    // Get form values
+    var name        = req.body.name;
+    var email       = req.body.email;
+    var body        = req.body.body;
+    var postid      = req.body.postid;
+    var commentdate = new Date();
+
+    // Form validation
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();
+    req.checkBody('email', 'Email is incorrectly formatted').isEmail();
+    req.checkBody('body', 'Body field is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        var posts = db.get('posts');
+        posts.findById(postid, function(err, post) {
+            res.render('show', {
+                "errors": errors,
+                "post": post
+            });
+        });
+    } else {
+        var comment = {
+            "name": name,
+            "email": email,
+            "body": body,
+            "commentdate": commentdate
+        };
+
+        var posts = db.get('posts');
+
+        posts.update({
+                "_id": postid
+            },
+            {
+                $push:{
+                    "comments": comment
+                }
+            },
+            function(err, doc) {
+                if(err) {
+                    throw err
+                } else {
+                    req.flash('success', 'Comment added');
+                    res.location('/posts/show/' + postid);
+                    res.redirect('/posts/show/' + postid);
+                }
+            }
+        );
+    }
+});
+
 module.exports = router;
