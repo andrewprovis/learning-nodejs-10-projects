86d56435592fe3866889af59f8425c7b95c69362
Andrew Provis <aprovis@dresources.com>
2015-07-09 09:35:05 +0100

    Created function to display a specific blog post.

--- section-5/routes/posts.js
+++ section-5/routes/posts.js
@@ -7,6 +7,15 @@ var router = express.Router();
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
+router.get('/show/:id', function(req, res, next) {
+    var posts = db.get('posts');
+    posts.findById(req.params.id, function(err, post) {
+        res.render('show', {
+            "post": post
+        });
+    });
+});
+
 router.get('/add', function(req, res, next) {
     var categories = db.get('categories');
 
