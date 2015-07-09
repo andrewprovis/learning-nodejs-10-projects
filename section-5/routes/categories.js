2b093376c888de9f88dc04569a5d44896428186b
Andrew Provis <aprovis@dresources.com>
2015-07-09 09:30:23 +0100

    Created a router that shows all posts by category when clicking on the posts category link.

--- section-5/routes/categories.js
+++ section-5/routes/categories.js
@@ -7,6 +7,17 @@ var router = express.Router();
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
+router.get('/show/:category', function(req, res, next) {
+    var db = req.db;
+    var posts = db.get('posts');
+    posts.find({category: req.params.category}, {}, function(err, posts) {
+        res.render('index', {
+            "title": req.params.category,
+            "posts": posts
+        });
+    });
+});
+
 router.get('/add', function(req, res, next) {
     res.render('addcategory', {
         "title": "Add Category"
